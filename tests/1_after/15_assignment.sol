afdce934781478ad12db7c66e11d7c534f1b6f8f
shafu <shafu0x@gmail.com>
2025-07-26 18:41:52 -0400

    Format variable assignments with storage/memory keywords

---
+++ tests/1_after/15_assignment.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Escrow {
+    function fund()  {
+        uint distributionId = distributionIds[i];
+        Distribution storage distribution = distributions     [distributionId];
+        RepoAccount  memory  repoAccount  = distributionToRepo[distributionId];
+    }
+}
